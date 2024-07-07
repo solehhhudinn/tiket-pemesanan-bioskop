759fe8bce7bd444ec5d01dfb6528aa8f9b9f70be
friham <alimcep@gmail.com>
2024-07-07 15:09:59 +0700

    create iklan admin

--- app/Http/Controllers/Admin/AdminIklanController.php
+++ app/Http/Controllers/Admin/AdminIklanController.php
@@ -15,4 +15,25 @@ class AdminIklanController extends Controller
         return view('admin.iklans.index', compact('iklans'));
     }
 
+    public function create()
+    {
+        return view('admin.iklans.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'iklan' => 'required|image',
+        ]);
+
+        $iklanPath = $request->file('iklan')->store('iklans', 'public');
+
+        Iklan::create([
+            'title' => $request->title,
+            'iklan' => $iklanPath,
+        ]);
+
+        return redirect()->route('admin.iklans.index')->with('success', 'Iklan created successfully.');
+    }
 }
